9d3c91833f91c4f4b0be07d4845c016f186d387a
Arne Taljaard <arnetaljaard@gmail.com>
2026-07-20 22:17:38 +0200

    update

--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -1,7 +1,9 @@
 import path from "path";
 
+// If TypeScript can't find the plugin's types in this environment, ignore the error.
+// @ts-ignore
 import react from "@vitejs/plugin-react";
-import { playwright } from "@vitest/browser-playwright";
+import { playwright } from "@vitest/browser";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
